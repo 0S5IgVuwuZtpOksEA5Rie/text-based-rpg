d588fcf90ab76ee7e19c8909771ab1e27514d487
Symbol <3qzwztfvcyvx6v@gmail.com>
2023-02-20 20:01:54 +1000

    something

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-fn main() {
+use std::io;
+
+fn intro_scene() {
     println!("Hello player, welcome to the game");
 
+    let user_name: String = input("What is your name?");
+}
 
+fn input(prompt: &str) -> String {
+    println!("{}", prompt);
+
+    let mut output: String = String::new();
+
+    io::stdin().read_line(&mut output).expect("read line error");
+
+    return output;
+}
+
+fn main() {
+    intro_scene();
 }
